ba19bd262bf224d3fd7e42af9a0524db61788ad9
frinhanifrinhani <frinhani.frinhani@gmail.com>
2017-11-30 15:09:12 -0200

    Primeiro comit, e essa porra nem funcionou

---
+++ src/main/java/br/com/javaspring_cc/loja/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package br.com.javaspring_cc.loja.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Carregando produtos");
+		return "killer";
+		
+	}
+
+}
